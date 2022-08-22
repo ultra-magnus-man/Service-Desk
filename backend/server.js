ff331fb96a409fa2a4baa8ece04754fa01fe4710
Manish Gupta <iammanishgupta1003@gmail.com>
2022-08-22 17:54:00 +0530

    Configurations for deployment

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
 const PORT = process.env.PORT || 5000;
 const connectDB = require("./config/db");
-
+const path = require("path");
 //connect to database
 connectDB();
 const app = express();
@@ -12,13 +12,27 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
-  res.send("Hello There!");
+  res.status(200).json({ message: "Welcome to Support Desk API" });
 });
 
 //Routes
 //We hve mentioned the path here that is why we don't need to mention the path in userRoutes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/tickets", require("./routes/ticketRoutes"));
+
+//Serve Frontend
+if (process.env.NODE_ENV === "production") {
+  //Set build folder as static
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.status(200).json({ message: "Welcome to Support Desk API" });
+  });
+}
 app.use(errorHandler);
 
 app.listen(PORT, () => {
